c7346ee5a76bee6ac685d30411ca04b259ef0d7f
Max C. Gluck <maxgluck@Maxs-MacBook.local>
2022-01-18 13:11:57 -0800

    Added code to keep track and react to the first die roll

--- CrapsRunner.java
+++ CrapsRunner.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class CrapsRunner {
     public static void main(String[] args) {
         
+        Die dice = new Die();
+        boolean win = false;
+        boolean lose = false;
+
+        if(true)
+        {
+            int firstRoll = dice.Roll();
+            if(firstRoll == 7 || firstRoll == 11)
+            {
+                win = true;
+            }
+            if(firstRoll == 2 || firstRoll == 3 || firstRoll == 12)
+            {
+                lose = true;
+            }
+
+        }
+        for(int x = 0; x < 1000; x++)
+        {
+            System.out.println(dice.Roll());
+        }
     }
 }
--- Die.java
+++ Die.java
@@ -1,17 +1,21 @@
 public class Die
 {
     //paramaters
-    private int num;
+    private int num1;
+    private int num2;
 
 
     //init variables
     public Die(){
-        num = (int)(Math.random() * 5) + 1;
+        num1 = 0;
+        num2 = 0;
     }
 
     //method
 
-    public int diceRoll(){
-        return num;
+    public int Roll(){
+        int num1 = (int)(Math.random() * 6) + 1;
+        int num2 = (int)(Math.random() * 6) + 1;
+        return num1 + num2;
     }
 }

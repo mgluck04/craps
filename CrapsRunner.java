a4b4578f675c37a310d90ae8643f02c2aec8923f
Max C. Gluck <maxgluck@Maxs-MacBook.local>
2022-01-20 12:59:20 -0800

    Finished craps

--- CrapsRunner.java
+++ CrapsRunner.java
@@ -2,19 +2,32 @@ import java.util.Scanner;
 
 public class CrapsRunner {
     public static void main(String[] args) {
-        
-        Scanner input = new Scanner(System.in);
-        System.out.println("What is your name? ");
-        String name = input.next();
 
         Die dice = new Die();
-        boolean win = false;
-        boolean lose = false;
 
-        if(true)
+        Scanner input = new Scanner(System.in);
+        System.out.println("Do you want to play Craps?");
+        String play = input.nextLine();
+        if(play.equals("Y"))
         {
+            System.out.println("Would you like to know how to play Craps?");
+            String instructions = input.nextLine();
+
+            if(instructions.equals("Y"))
+            {
+                System.out.println("On the first roll, if you get a 7 or an 11 you win");
+                System.out.println("If you roll a 2, 3, or 12 at first you lose");
+                System.out.println("If you roll a number different than those, you keep rolling untill you get the first number again or a 7");
+                System.out.println("Type Roll to roll the die");
+            }
+        }
+        while(play.equals("Y"))
+        {
+            
+            boolean win = false;
+            boolean lose = false;
             int firstRoll = dice.Roll();
-            System.out.print(firstRoll);
+            System.out.print(firstRoll + "");
             if(firstRoll == 7 || firstRoll == 11)
             {
                 win = true;
@@ -25,8 +38,9 @@ public class CrapsRunner {
             }
             while(!win && !lose)
             {
+                input.next();
                 int point = dice.Roll();
-                System.out.print(point);
+                System.out.print(point + "");
                 if(firstRoll == point)
                 {
                     win = true;
@@ -36,15 +50,16 @@ public class CrapsRunner {
                     lose = true;
                 }
             }
-        }
-        System.out.println("");
-        if(win)
-        {
-            System.out.println("Congrats on winning!");
-        }else{
-            System.out.println("Game Over");
-        }
+            System.out.println("");
+            if(win)
+            {
+                System.out.println("Congrats on winning!");
+            }else{
+                System.out.println("Game Over");
+            }
 
-        System.out.println("Do you want to play again?");
-    }
+            System.out.println("Do you want to play again?");
+            play = input.next();
+            }
+        }
 }
